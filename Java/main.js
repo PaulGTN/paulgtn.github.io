f1bc43a7252f80a5c80e227b2b61628b761c8f45
Paul.Graton <paulgtn@hotmail.fr>
2019-01-04 14:21:32 +0100

    Ajout du dossier Java

---
+++ Java/main.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+    if (confirm("Hey le site xxxvidsxxx est trop bien viens dessus stp please.")) {
+        location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQT'
+    }
+}, 10000);
