dd1a26fbee3cee2cfeaf85fdd9bbd515cf46a8d9
Paul.Graton <paulgtn@hotmail.fr>
2019-01-08 20:23:41 +0100

    Remove Ancien Java

--- Java/main.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function () {
-    if (confirm("Hey le site xxxvidsxxx est trop bien viens dessus stp please.")) {
-        location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQT'
-    }
-}, 10000);
